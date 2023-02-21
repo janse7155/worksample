cb380d555d2f582184a679301df6152cec36c784
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-02-21 10:40:57 -0600

    colors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// DOM Elements
+
 
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
@@ -6,7 +6,7 @@ const solarButton = document.getElementById('solar');
 const body = document.body;
 
 
-// Apply the cached theme on reload
+
 
 const theme = localStorage.getItem('theme');
 const isSolar = localStorage.getItem('isSolar');
@@ -16,7 +16,7 @@ if (theme) {
   isSolar && body.classList.add('solar');
 }
 
-// Button Event Handlers
+
 
 darkButton.onclick = () => {
   body.classList.replace('light', 'dark');
